63976d1bd9b3491906329df86691df6adf79d4db
Nemanja Rajkovic <n.rajkovic@levi9.com>
2018-12-08 02:43:23 +0100

    icons now move when screen size is changed

--- src/lib/top-bar/top-bar-module.ts
+++ src/lib/top-bar/top-bar-module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { A11yModule } from "@angular/cdk/a11y";
-import { LayoutModule } from "@angular/cdk/layout";
+import { LayoutModule, BreakpointObserver, MediaMatcher } from "@angular/cdk/layout";
 import { MatCommonModule }  from '@angular/material/core';
 import { MatSidenavModule }  from '@angular/material/sidenav';
 import { MatMenuModule } from "@angular/material/menu";
@@ -9,22 +9,28 @@ import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { MatTooltipModule } from "@angular/material/tooltip";
 import { MatTopBar } from "./top-bar";
+import { HostListener } from "@angular/core";
 
 @NgModule({
     imports: [
         CommonModule,
         A11yModule,
         LayoutModule,
-        LayoutModule,
         MatSidenavModule,
         MatMenuModule,
         MatIconModule,
         MatTooltipModule,
         MatButtonModule,
-        MatCommonModule,
+        MatCommonModule
+        
+    ],
+    providers: [
+        BreakpointObserver,
+        MediaMatcher,
+        HostListener
     ],
     exports: [ 
-        MatTopBar, MatCommonModule
+        MatTopBar, MatCommonModule        
     ],
     declarations: [
         MatTopBar
